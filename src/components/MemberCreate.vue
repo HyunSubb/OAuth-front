e7085146b5fcf88bdb4bd96b4915364c87772e16
신현섭 <didakd22@naver.com>
2025-06-26 01:36:28 +0900

    first commit

---
+++ src/components/MemberCreate.vue
@@ -0,0 +1,10 @@
+<template>
+    <!-- 여기에 HTML 템플릿(화면에 보이는 부분)을 작성합니다 -->
+</template>
+
+<script>
+// export default는 이 컴포넌트를 다른 곳에서 import해서 사용할 수 있게 해줍니다.
+export default {
+    // 컴포넌트의 옵션(data, methods, props 등)이 들어갑니다.
+};
+</script>
