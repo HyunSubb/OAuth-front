3c39d29bd618e0ccaa93cab3d6b6ebc5afc25e0f
신현섭 <didakd22@naver.com>
2025-06-26 04:23:13 +0900

    chapter/3-12 완료

--- src/components/MemberCreate.vue
+++ src/components/MemberCreate.vue
@@ -1,11 +1,46 @@
 <template>
     <!-- 여기에 HTML 템플릿(화면에 보이는 부분)을 작성합니다 -->
     회원가입 화면입니다..
+    <v-container>
+        <v-row justify="center">
+            <v-col md="6">
+                <v-card>
+                    <v-card-title class="text-h5 text-center"> 회원가입 </v-card-title>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field label="email" v-model="email"> </v-text-field>
+                            <v-text-field label="password" v-model="password" type="password"> </v-text-field>
+                            <v-btn type="button" color="primary" block @click="memberCreate()">등록</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 // export default는 이 컴포넌트를 다른 곳에서 import해서 사용할 수 있게 해줍니다.
 export default {
     // 컴포넌트의 옵션(data, methods, props 등)이 들어갑니다.
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        async memberCreate() {
+            const registerData = {
+                email: this.email,
+                password: this.password,
+            };
+
+            await axios.post("http://localhost:8080/member/create", registerData);
+            window.location.href = "/";
+        },
+    },
 };
 </script>
--- src/components/MemberLogin.vue
+++ src/components/MemberLogin.vue
@@ -1,11 +1,61 @@
 <template>
     <!-- 여기에 HTML 템플릿(화면에 보이는 부분)을 작성합니다 -->
-    로그인 화면 입니다..
+    로그인 화면입니다..
+    <v-container>
+        <v-row justify="center">
+            <v-col md="4">
+                <v-card>
+                    <v-card-title class="text-h5 text-center"> 로그인 </v-card-title>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field label="email" v-model="email"> </v-text-field>
+                            <v-text-field label="password" v-model="password" type="password"> </v-text-field>
+                            <v-btn type="button" color="primary" block @click="memberLogin()">로그인</v-btn>
+                        </v-form>
+                        <br />
+                        <v-row>
+                            <v-col cols="6" class="d-flex justify-center">
+                                <img src="@/assets/Google_Login.png" style="max-height: 40px; width: auto" @click="googleLogin()" />
+                            </v-col>
+                            <v-col cols="6" class="d-flex justify-center">
+                                <img src="@/assets/Kakao_Login.png" style="max-height: 40px; width: auto" @click="kakaoLogin()" />
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 // export default는 이 컴포넌트를 다른 곳에서 import해서 사용할 수 있게 해줍니다.
 export default {
     // 컴포넌트의 옵션(data, methods, props 등)이 들어갑니다.
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        async memberLogin() {
+            const loginData = {
+                email: this.email,
+                password: this.password,
+            };
+
+            const response = await axios.post("http://localhost:8080/member/doLogin", loginData);
+            // response는 내부에 data가 있는데 거기에서 내용들을 확인한다.
+            const token = response.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "/";
+        },
+
+        googleLogin() {},
+        kakaoLogin() {},
+    },
 };
 </script>
